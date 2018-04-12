27e8cb415bc1324d31bb114d28bf5f42c20d1113
Isael Lizama <isaelwebdeveloper@gmail.com>
2018-04-12 10:22:15 -0600

    added store add and delete

--- scripts/store.js
+++ scripts/store.js
@@ -1,6 +1,19 @@
 'use strict';
 const Store = (function(){
   
-  
+  const store = [];
+
+  const addBookmark = function(bookmark){
+    this.store.push(bookmark);
+  };
+
+  const deleteBookmark = function(id){
+    this.store = this.store.filter(bookmark => id !== bookmark);
+  };
+
+  return {
+    store,
+    addBookmark,
+  };
 
 }());
