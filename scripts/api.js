18ed5edb80abc4d9adf6c87a0be89ad481bc53c8
Isael Lizama <isaelwebdeveloper@gmail.com>
2018-04-12 10:31:43 -0600

    created getBookmarks func and test with store AddItems

--- scripts/api.js
+++ scripts/api.js
@@ -1,8 +1,22 @@
 'use strict';
 const Api = (function(){
   
-  const getBookmarks = function(callback){
+  const URL = 'https://thinkful-list-api.herokuapp.com/isael/bookmarks';
 
+  const getBookmarks = function(callback){
+    const options = {
+      url: URL,
+      method: 'GET',
+      contentType: 'application/json',
+      success: callback,
+    };
+
+    $.ajax(options);
+  };
+  //Api.getBookmarks(response => response.forEach(item => Store.addBookmark(item)));
+
+  const addBookmark = function(dataObj, callback){
+    
   };
 
   const updateBookmark = function(id, dataObj, callback){
