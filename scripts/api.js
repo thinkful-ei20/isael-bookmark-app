da358d5c2f1e8d6a9cba0f9dcde5d1774d3c3d28
Isael Lizama <isaelwebdeveloper@gmail.com>
2018-04-12 10:20:18 -0600

    created api stubs and iffys

---
+++ scripts/api.js
@@ -0,0 +1,22 @@
+'use strict';
+const Api = (function(){
+  
+  const getBookmarks = function(callback){
+
+  };
+
+  const updateBookmark = function(id, dataObj, callback){
+
+  };
+
+  const deleteBookmark = function(id, callback){
+
+  };
+
+  return {
+    getBookmarks,
+    updateBookmark,
+    deleteBookmark,
+  };
+
+}());
---
+++ scripts/bookmark.js
@@ -0,0 +1,12 @@
+'use strict';
+const Bookmark = (function(){
+  
+  const foo = function(){
+    console.log("hello");
+  };
+
+  return {
+    foo,
+  };
+
+}());
